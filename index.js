ccaefee3896091b78483b8d3f85089029172e89f
Pamela Bortnick <pbortnick@gmail.com>
2018-02-27 04:05:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function searchRepositories() {
     var searchTerms = $("#searchTerms").val()
     var url = "https://api.github.com/search/repositories?q=" + searchTerms
     $.get(url, function(data) {
-        // debugger
        $("#results").html(renderResults(data));
     }).fail(function(error) {
       // This is called when an error occurs
